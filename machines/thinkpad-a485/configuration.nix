b24f65be99e99d87a1b7c510dce78cc6463b47ea
Miroslav Kravec <kravec.miroslav@gmail.com>
2022-12-17 08:58:55 +0100

    Initial ThinkPad A485 installation

---
+++ machines/thinkpad-a485/configuration.nix
@@ -0,0 +1,75 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    ./hardware-configuration.nix
+
+    ../../profiles/cli.nix
+  ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+
+  boot.initrd.secrets = {
+    "/crypto_keyfile.bin" = null;
+  };
+
+  networking.hostName = "miroslav-thinkpad";
+  networking.networkmanager.enable = true;
+
+  time.timeZone = "Europe/Bratislava";
+
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "sk_SK.UTF-8";
+    LC_IDENTIFICATION = "sk_SK.UTF-8";
+    LC_MEASUREMENT = "sk_SK.UTF-8";
+    LC_MONETARY = "sk_SK.UTF-8";
+    LC_NAME = "sk_SK.UTF-8";
+    LC_NUMERIC = "sk_SK.UTF-8";
+    LC_PAPER = "sk_SK.UTF-8";
+    LC_TELEPHONE = "sk_SK.UTF-8";
+    LC_TIME = "sk_SK.UTF-8";
+  };
+
+  services.xserver.enable = true;
+
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  services.xserver = {
+    layout = "us";
+    xkbVariant = "";
+  };
+
+  services.printing.enable = true;
+
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+
+    alsa.enable = true;
+    alsa.support32Bit = true;
+
+    pulse.enable = true;
+  };
+
+  users.users.miroslav = {
+    isNormalUser = true;
+    description = "Miroslav Kravec";
+    extraGroups = [ "networkmanager" "wheel" ];
+  };
+
+  nixpkgs.config.allowUnfree = true;
+
+  environment.systemPackages = with pkgs; [
+    google-chrome
+    signal-desktop
+  ];
+
+  system.stateVersion = "22.11";
+}
