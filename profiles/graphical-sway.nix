b50f6c2683c00dfd6e5ed19e29a3a6f222986233
Miroslav Kravec <kravec.miroslav@gmail.com>
2023-02-12 12:02:52 +0100

    Use systemd user services instead of sway exec

--- profiles/graphical-sway.nix
+++ profiles/graphical-sway.nix
@@ -31,5 +31,46 @@
 
   environment.etc."sway/config".text = (builtins.readFile ../etc/sway/config);
   environment.etc."i3status.conf".text = (builtins.readFile ../etc/i3status.conf);
+  environment.etc."xdg/swayidle/config".text = (builtins.readFile ../etc/xdg/swayidle/config);
   environment.etc."xdg/waybar/config".text = (builtins.readFile ../etc/xdg/waybar/config);
+
+  # see: https://github.com/swaywm/sway/wiki/Systemd-integration
+  systemd.user.targets.sway-session = {
+    description = "sway compositor session";
+    documentation = [ "man:systemd.special(7)" ];
+    bindsTo = [ "graphical-session.target" ];
+    wants = [ "graphical-session-pre.target" ];
+    after = [ "graphical-session-pre.target" ];
+  };
+
+  systemd.user.targets.tray = {
+    description = "System Tray";
+    requires = [ "graphical-session-pre.target" ];
+  };
+
+  systemd.user.services.kanshi = {
+    bindsTo = [ "sway-session.target" ];
+    wantedBy = [ "sway-session.target" ];
+
+    serviceConfig.Restart = "always";
+    serviceConfig.RestartSec = "3";
+
+    script = "${pkgs.kanshi}/bin/kanshi";
+  };
+
+  systemd.user.services.swayidle = {
+    bindsTo = [ "sway-session.target" ];
+    wantedBy = [ "sway-session.target" ];
+
+    serviceConfig.Restart = "always";
+    serviceConfig.RestartSec = "3";
+
+    script = "${pkgs.swayidle}/bin/swayidle -C /etc/xdg/swayidle/config";
+
+    path = with pkgs; [
+      sway
+      swaylock
+    ];
+  };
+
 }
