998ad180696302e4207827c3bd543f1930bfd42a
Miroslav Kravec <kravec.miroslav@gmail.com>
2023-10-07 10:34:56 +0200

    ThinkPad A485 reinstall (SSD capacity upgrade)

--- machines/thinkpad-a485/configuration.nix
+++ machines/thinkpad-a485/configuration.nix
@@ -18,10 +18,9 @@
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.efi.efiSysMountPoint = "/boot/efi";
 
-  boot.initrd.secrets = {
-    "/crypto_keyfile.bin" = null;
+  boot.initrd.luks.devices = {
+    "enc-system".device = "/dev/disk/by-uuid/27de2117-d606-4d91-87e2-462369ca244a";
   };
 
   boot.initrd.availableKernelModules = [
@@ -60,5 +59,11 @@
     jetbrains.webstorm
   ];
 
-  system.stateVersion = "22.11";
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It's perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.05"; # Did you read the comment?
 }
--- machines/thinkpad-a485/hardware-configuration.nix
+++ machines/thinkpad-a485/hardware-configuration.nix
@@ -9,23 +9,23 @@
     ];
 
   boot.initrd.availableKernelModules = [ "nvme" "ehci_pci" "xhci_pci" "ahci" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
-  boot.initrd.kernelModules = [ ];
+  boot.initrd.kernelModules = [ "dm-snapshot" ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/95da975f-d16d-44ce-9235-ca87f19b6504";
+    { device = "/dev/disk/by-uuid/14cd9e98-ab5d-4a07-9e53-7ea6d754666e";
       fsType = "ext4";
     };
 
-  boot.initrd.luks.devices."luks-34fea9d6-094b-4993-8b84-d1443406e79a".device = "/dev/disk/by-uuid/34fea9d6-094b-4993-8b84-d1443406e79a";
-
-  fileSystems."/boot/efi" =
-    { device = "/dev/disk/by-uuid/7A65-92C7";
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/5E91-4D59";
       fsType = "vfat";
     };
 
-  swapDevices = [ ];
+  swapDevices =
+    [ { device = "/dev/disk/by-uuid/601f5bdf-9a6e-497a-a87c-513809de8eeb"; }
+    ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
